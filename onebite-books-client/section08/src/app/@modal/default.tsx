2ab72b082ca4dcf5c979abdf3802ebd7699a5b63
HY KIM <chopinoff@gmail.com>
2025-02-15 08:16:10 +0900

    Feat: Onebite Book App Router Routing Pattern

--- onebite-books-client/section08/src/app/@modal/default.tsx
+++ onebite-books-client/section08/src/app/@modal/default.tsx
@@ -1,7 +1,3 @@
-// 만약 / (index 페이지)로 접속한다면?
-// @modal에는 / 이 페이지가 없기 때문에 404가 뜰 것임
-// 그래서 default.tsx를 만들어줘야 함
-// 그냥 index로 접속할 때는 아무것도 띄우지 않도록 null
 export default function Default() {
     return null;
 }
--- onebite-books-client/section08/src/app/layout.tsx
+++ onebite-books-client/section08/src/app/layout.tsx
@@ -31,11 +31,6 @@ export default function RootLayout({
     children: React.ReactNode;
     modal: ReactNode;
 }>) {
-    // 정리
-    // /로 접속: children은 (@with-searchbar)\page.tsx, modal은 @modal\default.tsx
-    // book/1로 접속: children은 (.)book\[id]\page.tsx가 아니라 왜 index.tsx를 유지?
-    // > 인터셉팅 라우트가 modal에 이미 존재하므로 원래는 book\[id]\page.tsx로 가야하는 걸 뺏기고 그냥 예전 페이지가 뜨는 것..? 일단 이렇게 외우자
-    // modal은 (.)book\[id]\page.tsx
     return (
         <html lang="en">
             <body>
